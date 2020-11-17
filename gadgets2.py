0882b32f71df274acaf548e52ecce815fff58b50
Dillon J Brownell <73907611+Dillon214@users.noreply.github.com>
2020-11-17 15:55:55 -0800

    Update gadgets2.py

--- gadgets2.py
+++ gadgets2.py
@@ -1,8 +1,16 @@
 
-
+#depthling is meant to clone an array or array of arrays into a new variable. This probably exists as some command, but I wanted to make it anyways bacause I was sick of my variables changing whe they weren't supposed to. 
 def depthling(input):
 
     if type(input[0]) == type(0):
         return (input[:])
     return ([depthling(x) for x in input])
+
+
+
+
+
+
+
+
 
